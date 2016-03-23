084b92dd14272f6b8ced6ffaf13a9e98ab9aeb86
Jeff Bachtel <jeffrey.bachtel@stelligent.com>
2016-03-23 14:40:15 -0400

    Comment shell script

--- packer/create-image.sh
+++ packer/create-image.sh
@@ -6,8 +6,10 @@
 AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID:-""}
 AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY:-""}
 
+# Validate that the ZAP Packer template is conformant
 packer validate ./zap-ami-packer.json
 
+# Launch the Packer build of the ZAP AMI
 #export PACKER_LOG=1
 packer build -var aws_access_key=${AWS_ACCESS_KEY_ID} \
              -var aws_secret_key=${AWS_SECRET_ACCESS_KEY} \
