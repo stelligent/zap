64f1c0e97a8c7097f83331835ce3bc7df1724f07
Jeff Bachtel <jeffrey.bachtel@stelligent.com>
2016-03-23 14:36:15 -0400

    Remove not really required env vars

--- packer/create-image.sh
+++ packer/create-image.sh
@@ -1,15 +1,5 @@
 #!/bin/bash -e
 
-required_env_vars=(AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY)
-for required_env_var in "${required_env_vars[@]}";
-do
-  if [[ -z ${!required_env_var} ]];
-  then
-    echo must set ${required_env_var}
-    exit 1
-  fi
-done
-
 packer validate ./zap-ami-packer.json
 
 #export PACKER_LOG=1
