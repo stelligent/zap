b5ea7638bf2a520e7f7dacd614caf95a4a6540d8
Jeff Bachtel <jeffrey.bachtel@stelligent.com>
2016-03-23 16:01:53 -0400

    Allow VPC and subnet to be specified for packer

--- packer/create-image.sh
+++ packer/create-image.sh
@@ -6,6 +6,11 @@
 AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID:-""}
 AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY:-""}
 
+# Default VPC for building
+PACKER_VPC=${PACKER_VPC:-vpc-f399e097}
+# Default VPC subnet for building
+PACKER_SUBNET=${PACKER_SUBNET:-subnet-d68818eb}
+
 # Validate that the ZAP Packer template is conformant
 packer validate ./zap-ami-packer.json
 
@@ -13,4 +18,6 @@ packer validate ./zap-ami-packer.json
 #export PACKER_LOG=1
 packer build -var aws_access_key=${AWS_ACCESS_KEY_ID} \
              -var aws_secret_key=${AWS_SECRET_ACCESS_KEY} \
+             -var vpc_id_to_build_ami_in=${PACKER_VPC} \
+             -var subnet_id_to_build_ami_in=${PACKER_SUBNET} \
              ./zap-ami-packer.json
